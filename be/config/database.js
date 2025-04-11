817f17fd3dd0e3860c176db5191f6d6689d1d2e3
unknown <syifanurramadhani54@gmail.com>
2025-04-11 10:02:13 +0700

    ubah db

--- be/config/database.js
+++ be/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("notes", "root", "", {
+const db = new Sequelize("RECOVER_YOUR_DATA ", "root", "", {
     host: "35.239.47.213",
     dialect: "mysql"
 })
